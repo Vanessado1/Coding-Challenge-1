139be615983dbdf0e2f2e50ec254feef722c7a91
Vanessa Do <xex.vanessa@gmail.com>
2025-01-26 18:09:19 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,11 @@ const employeeID = 100;
 console.log(`Employee ID: ${employeeID}, type: ${typeof employeeID}`);// Output: Employee ID: 100, type: number
 var isActive = true;
 console.log(`isActive: ${isActive}, type: ${typeof isActive}`);// Output: isActive: true, type: boolean
+
+//Task 2: Primitive Data Types
+let productName = 'Ducky';
+console.log(`Product Name: ${productName}, type: ${typeof productName}`);// Output: Product Name: Ducky, type: string
+const productPrice = 12.99;
+console.log(`Product Price: ${productPrice}, type: ${typeof productPrice}`);// Output: Product Price: 12.99, type: number
+var isAvailable = true;
+console.log(`isAvailable: ${isAvailable}, type: ${typeof isAvailable}`);// Output: isAvailable: true, type: boolean
