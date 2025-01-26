d82365c1db15e8f0b14f1c77446aa779a72fe897
Vanessa Do <xex.vanessa@gmail.com>
2025-01-26 18:06:09 -0500

    Task 1- Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+//Task 1: Variables
+let employeeName = 'Mary';
+console.log(`Employee Name: ${employeeName}, type: ${typeof employeeName}`);// Output: Employee Name: Mary, type: string
+const employeeID = 100;
+console.log(`Employee ID: ${employeeID}, type: ${typeof employeeID}`);// Output: Employee ID: 100, type: number
+var isActive = true;
+console.log(`isActive: ${isActive}, type: ${typeof isActive}`);// Output: isActive: true, type: boolean
